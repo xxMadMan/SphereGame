e077ffd5d9bfe7ec81706c72b943a26591f68b66
Kaleb <kalebismadman@gmail.com>
2020-07-07 23:41:46 +1000

    Inventory, Items, Equipping
    Working on the items for the inventory, items can now be equipped and unequipped from the player, and objects will spawn according to code design.

---
+++ Assets/Scripts/Prototype/Equipment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item
+{
+    public EquipmentSlot equipSlot;
+    public GameObject equipmentObj;
+
+    public int armourModifier;
+    public int damageModifier;
+
+    public override void Use()
+    {
+        base.Use();
+        EquipmentManager.instance.Equip(this);
+        RemoveFromInventory();
+    }
+
+}
+
+public enum EquipmentSlot { Head, Chest, Legs, Weapon, Shield, Feet}
---
+++ Assets/Scripts/Prototype/EquipmentManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour
+{
+
+    #region Singleton
+    public static EquipmentManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject targetObj;
+    Equipment[] currentEquipment;
+    GameObject[] currentObj;
+
+    public delegate void OnEquipmentChanged(Equipment newItem, Equipment oldItem);
+    public OnEquipmentChanged onEquipmentChanged;
+
+    Inventory inventory;
+
+    void Start()
+    {
+        inventory = Inventory.instance;
+
+        int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
+        currentEquipment = new Equipment[numSlots];
+        currentObj = new GameObject[numSlots];
+    }
+
+    public void Equip(Equipment newItem)
+    {
+        int slotIndex = (int)newItem.equipSlot;
+
+        Equipment oldItem = null;
+
+        if (currentEquipment[slotIndex] != null)
+        {
+            oldItem = currentEquipment[slotIndex];
+            inventory.Add(oldItem);
+            Destroy(currentObj[slotIndex].gameObject);
+        }
+
+        if (onEquipmentChanged != null)
+        {
+            onEquipmentChanged.Invoke(newItem, oldItem);
+        }
+
+        currentEquipment[slotIndex] = newItem;
+        GameObject newObj = Instantiate<GameObject>(newItem.equipmentObj);
+        newObj.transform.parent = targetObj.transform;
+        newObj.transform.position = targetObj.transform.position;
+
+        currentObj[slotIndex] = newObj;
+    }
+
+    public void Unequip (int slotIndex)
+    {
+        if (currentEquipment[slotIndex] != null)
+        {
+            if (currentObj [slotIndex] != null)
+            {
+                Destroy(currentObj[slotIndex].gameObject);
+            }
+
+            Equipment oldItem = currentEquipment[slotIndex];
+            inventory.Add(oldItem);
+
+            currentEquipment[slotIndex] = null;
+
+            if (onEquipmentChanged != null)
+            {
+                onEquipmentChanged.Invoke(null, oldItem);
+            }
+        }
+    }
+
+    public void UnequipAll()
+    {
+        for (int i = 0; i < currentEquipment.Length; i++)
+        {
+            Unequip(i);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+            UnequipAll();
+    }
+}
