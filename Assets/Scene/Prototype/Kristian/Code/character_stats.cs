9d8d8c6d223bd185159c172bcaf1cde7273eba8b
Kristian <kristian.gaedt@gmail.com>
2020-07-14 23:32:31 +0930

    JSON file testing
    Prototyping the ability to create stats based on json files

---
+++ Assets/Scene/Prototype/Kristian/Code/character_stats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using System.IO;
+
+public class character_stats : MonoBehaviour
+{
+    public Object jsonFile;
+
+    public string name;
+    public int hp;
+    
+    void Start()
+    {
+        Test StatsTest = JsonUtility.FromJson<Test>(jsonFile.ToString());
+        
+        name = StatsTest.name;
+        hp = StatsTest.hp;
+
+        Debug.Log(StatsTest.name);
+    }
+}
+
+[System.Serializable]
+public class Test
+{
+    public string name;
+    public int hp;
+}
